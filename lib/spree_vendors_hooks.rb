839d5e2557404a031d13c0c298c3f2b1dc9cac05
Антон <antonio@antonio-P5KC.(none)>
2011-10-31 04:05:57 +0400

    Add Rus

--- lib/spree_vendors_hooks.rb
+++ lib/spree_vendors_hooks.rb
@@ -1,6 +1,6 @@
 class SpreeVendorsHooks < Spree::ThemeSupport::HookListener
   insert_after :admin_product_sub_tabs do
-    %(<%= tab :vendors %>)
+    %(<%= tab (:vendors ) %>)
   end
   insert_after :admin_products_index_search, :partial => 'admin/products/vendor_search_field'
   insert_after :admin_product_form_right,    :partial => 'admin/products/vendor_field'
@@ -9,3 +9,4 @@ class SpreeVendorsHooks < Spree::ThemeSupport::HookListener
   insert_before :admin_shipments_index_rows,    :partial => 'admin/shipments/vendor_shipments_index_rows'
   insert_before :admin_shipment_edit_header,    :partial => 'admin/shipments/vendor_shipment_edit_header'
 end
+
