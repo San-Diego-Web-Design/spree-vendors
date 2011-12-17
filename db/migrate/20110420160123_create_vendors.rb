58cc466bbfd26f79ac52866e3e5f8d99ac6421f3
Антон <antonio@antonio-P5KC.(none)>
2011-12-18 01:21:49 +0400

    url_vendor

--- db/migrate/20110420160123_create_vendors.rb
+++ db/migrate/20110420160123_create_vendors.rb
@@ -7,11 +7,15 @@ class CreateVendors < ActiveRecord::Migration
     end
 
     add_column :products, :vendor_id, :integer
+    add_column :products, :product_vendor_url, :string
+    add_column :products, :product_vendor_url_2, :string
     add_column :shipments, :vendor_id, :integer
   end
 
   def self.down
     remove_column :products, :vendor_id
+    remove_column :products, :product_vendor_url, :string
+    remove_column :products, :product_vendor_url_2, :string
     remove_column :shipments, :vendor_id
     drop_table :vendors
   end
--- lib/spree_vendors_hooks.rb
+++ lib/spree_vendors_hooks.rb
@@ -5,8 +5,11 @@ class SpreeVendorsHooks < Spree::ThemeSupport::HookListener
   insert_after :admin_products_index_search, :partial => 'admin/products/vendor_search_field'
   insert_after :admin_product_form_right,    :partial => 'admin/products/vendor_field'
 
+  insert_before :admin_product_form_meta,    :partial => 'admin/products/product_vendor_url'
+
+  insert_after :admin_order_show_details,    :partial => 'admin/products/product_vendor_order'
+
   insert_before :admin_shipments_index_headers, :partial => 'admin/shipments/vendor_shipments_index_headers'
   insert_before :admin_shipments_index_rows,    :partial => 'admin/shipments/vendor_shipments_index_rows'
   insert_before :admin_shipment_edit_header,    :partial => 'admin/shipments/vendor_shipment_edit_header'
 end
-
